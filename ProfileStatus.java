4b4b7495b9008450e91e47fcbbe3166571445927
Ezekiel, Gwun-orene R <lumgwun1@gmail.com>
2019-12-09 07:22:02 -0800

    Add files via upload

--- ProfileStatus.java
+++ ProfileStatus.java
@@ -1,18 +1,4 @@
-package group.lsg.resultinvestmentapp.Class;
+package group.lsg.resultinvestmentapp.Enum;
 
 public enum ProfileStatus {
-        PROFILE_CREATED(0), NOT_AUTHORIZED(1), NO_PROFILE(2), IS_ADMIN(3),
-        IS_INVESTOR(4), IS__SUPER_ADMIN(5), IS_AUTHORIZED(6);
-
-        int status;
-
-        ProfileStatus(int status) {
-            this.status = status;
-        }
-
-        public int getStatus() {
-            return status;
-
-        }
-    }
-
+}
